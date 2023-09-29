2121aeb1f2c513be5e613b2c285381f2b7382af4
Natalie Decker <ilikepegasi@gmail.com>
2023-09-29 16:20:53 -0500

    stuff

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print(9534 % 10)
+import random
+def main():
+    target = random.randint(1, 10)
+    guess = int(input("Enter an integer: "))
+    if target > guess:
+        print("Too high")
+    elif target < guess:
+        print("Too low")
+    else:
+        print("Equal")
+    print(f"The target was {target}")      
+        
+main()
