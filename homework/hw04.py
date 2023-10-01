565488bb38d4d06411597eec132232af27839c3c
Natalie Decker <ilikepegasi@gmail.com>
2023-10-01 15:48:24 -0500

    halfway done with problem 4

--- homework/hw04.py
+++ homework/hw04.py
@@ -56,8 +56,11 @@ def three_options(text, optionA, optionB, optionC):
     return choice
 
 def adventure():
+    '''
+    a
+    '''
     prompt1 = "Intense lightning strikes down from above. Your old mentor looks upon you. You never thought he would do this, but now you are falling towards the windswept rocks below. You need to stop yourself from crashing down, but how? "
-    option1a = "Remember your allies, the friends you made along the way. Call for the noble Asa to save you, with your golden whistle."
+    option1a = "Remember your allies, the friends you made along the way. Call for the noble Daisy to save you, with your golden whistle."
     option1b = "You know there is still some good in him. Reach out to your mentor and ask him to remember"
     option1c = "Call upon your deep wellspring of magical power and will yourself back onto the top of the cliff."
     choice1 = three_options(prompt1, option1a, option1b, option1c)
@@ -65,10 +68,36 @@ def adventure():
 
     if(choice1 == "A"):
         print("The puppy you raised from birth, now your noble flying daschund Daisy, swoops down to save your hide just before you are dashed on the rocks. You let a tear fall for your mentor before flying away.")
+        flag2 = False
+        flag3 = False
+        flag4 = False
     elif(choice1 == "B"):
         print("His face is pushed away, as if by some unseen force, but something comes over his face. He tosses a rope down to you and you climb back up. But as you look to him, he scowls. The moment has passed.")
+        flag2 = True
+        flag3 = False
+        flag4 = False
     else:
         print("Your magic is nearly emptied after your long trek to the cliffs, but you manage to push through, and you are returned to the cliff you just were pushed off of- by a the man who still stands there.")
+        flag2 = False
+        flag3 = False
+        flag4 = True
+
+    if flag2:
+        prompt2 = "He helped you up... But will he keep helping you? Will you trust him, or take this oppurtunity to end his threat?"
+        option2a = "He still has good in him, he showed that. Reach out a hand in kindness."
+        option2b = "In his moment of weakness, now you can push him off the cliff."
+        option2c = "You need to run, get far away from this place and warn the others."
+        choice2 = three_options(prompt2, option2a, option2b, option2c)
+    else:
+        choice2 = 0
+    
+    if(choice2 == "A"):
+        print()
+
+            
+
+
+
 
 if __name__ == "__main__":
     
