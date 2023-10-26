024037bc111527d485f46c10664bb61fd57fc80e
ilikepegasi <74635574+ilikepegasi@users.noreply.github.com>
2023-10-26 20:01:07 +0000

    documented hw08

--- homework/hw08/hw08.py
+++ homework/hw08/hw08.py
@@ -44,8 +44,21 @@ def correct_word(text):
     return "???"
 
 def correct_all(sentence):
+    '''
+    Purpose:
+        Corrects the spellings of each words in a sentence by checking similarity to the top 1000
+        words in the English language. If none can be found, the word is replaced with ???.
+        It assumes no capitalization other than for the first letter and no punctuation other 
+        than a final period.
+    Parameters:
+        sentence: (str) the sentence to be spellchecked 
+    Return Value: (str)
+        The sentence with each of its words replaced with its closest equivalent in the top 1000 
+        words of the English language. If no word is found, it is replaced with ???. The sentence 
+        will be all lowercase, except for the first letter. It ends with one period. 
+    '''
     sentence = sentence.replace(".", "")
-    words = sentence.split()
+    words = sentence.lower().split()
     words[0] = words[0].lower()
     for i, word in enumerate(words):
         words[i] = correct_word(word)
@@ -54,12 +67,50 @@ def correct_all(sentence):
     words += "."
     return words
 
+def correct_word2(text):
+    '''
+    Purpose: 
+        Finds a word in the top 1000 most common words in the 
+        English language closest to your word
+    Parameter(s):
+        text: (str) A string of lowercase letters representing the
+        English word we want to correct.
+    Return Value:
+        (str) A word found by making a single letter change to your given word, until your word matches
+        if one can be found in words.txt. Or '???' if not.
+    '''
+    common_words = open_word_file()
+    if common_words.count(text) != 0:
+        return text
+    for shared_letters_limit in range(1, len(text)):
+        for word in common_words:
+            if len(word) == len(text):
+                shared_letters = 0
+                for i, letter in enumerate(text):
+                    if word[i] == letter:
+                        shared_letters += 1
+                if shared_letters >= (len(text) - shared_letters_limit):
+                    return word
+    return "???"
+
+def correct_all2(sentence):
+    sentence = sentence.replace(".", "")
+    words = sentence.lower().split()
+    words[0] = words[0].lower()
+    for i, word in enumerate(words):
+        words[i] = correct_word2(word)
+    words[0] = words[0][0].upper() + words[0][1:]
+    words = " ".join(words)
+    words += "."
+    return words
+
+
 if __name__ == '__main__':
     #Should output You shall not pass.
-    print(correct_all('Bou shale net mass.'))
+    print(correct_all2('Bou shale net mass.'))
 
     #Should output ??? of these ??? ??? ???.
-    print(correct_all('Alll sf tgese wrds haev typos.'))
+    print(correct_all2('Alll sf tgese wrds haev typos.'))
     
     #Should output More words not thus time they had be fixed.
-    print(correct_all('More sords bot thus timm whey cad be foxed.'))
+    print(correct_all2('More sords bot thus timm whey cad be foxed.'))
