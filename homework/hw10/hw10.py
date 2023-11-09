5458e5a454136bca0c7932391a0bb09b23bdba2f
ilikepegasi <74635574+ilikepegasi@users.noreply.github.com>
2023-11-09 00:45:35 +0000

    completed hw10 problem 2

--- homework/hw10/hw10.py
+++ homework/hw10/hw10.py
@@ -7,5 +7,13 @@ def total_cost(order:dict, prices:dict):
         total_price += prices[key] * order[key]
     return total_price
 
-def follows(text: string):
-    print()
+def follows(text: str):
+    #TODO add docstring
+    text = text.split()
+    following = {}
+    for word in text:
+        following[word] = []
+    for i, word in enumerate(text):
+        if i != len(text) - 1:
+            following[word].append(text[i + 1])
+    print(following)
--- testLexture.py
+++ testLexture.py
@@ -1,10 +1,6 @@
-def testFunc(a):
-    a = "alpha"
-    return a
 def main():
-    a = "epsilon"
-    b = [[[]]]
-    testFunc(a)
+    a = {'x':3}
+    a["x"] = 4
     print(a)
 if __name__ == "__main__":
     main()
