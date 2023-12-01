f9eea40c2ea57e451306eaef59edd4d3c8e15e76
Natalie Decker <ilikepegasi@gmail.com>
2023-11-30 20:23:24 -0600

    wowee broken gravity

--- labs/lab12/nbodyproblem.py
+++ labs/lab12/nbodyproblem.py
@@ -2,27 +2,32 @@ from combined import Vec2
 from combined import Particle
 import math
 import random
-BIG_G = 100
-TIME_STEP = 0.001
+BIG_G = 10
+TIME_STEP = 0.1
 def gravity(bodyA:Particle, bodyB:Particle) -> Vec2:
     distance_x = bodyA.pos.x - bodyB.pos.x
     distance_y = bodyA.pos.y - bodyB.pos.y
     distance = (distance_x**2 + distance_y**2)**0.5
     try:
-        force_magnitude = (BIG_G * bodyA.mass * bodyB.mass) / (distance**2)
+        force_magnitude_x = (BIG_G * bodyA.mass * bodyB.mass) / (distance_x**2)
     except ZeroDivisionError:
-        force_magnitude = 0
-    force_direction = math.atan(distance_y / distance_x)
-    force_vector = Vec2(force_magnitude*math.cos(force_direction), force_magnitude*math.sin(force_direction))
+        force_magnitude = 1
+    try:
+        force_magnitude_y = (BIG_G * bodyA.mass * bodyB.mass) / (distance_y**2)
+    except ZeroDivisionError:
+        force_magnitude = 1
+    force_vector = Vec2(force_magnitude_x, force_magnitude_y)
     return force_vector
 
 def simulate(bodies:list) -> None:
+    for body in bodies:
+        body.force.set_values([0, 0])
     for i in range(0, len(bodies)-1):
         for j in range(i+1, len(bodies)):
             calculated_force = gravity(bodies[i], bodies[j])
             print(calculated_force)
             bodies[i].force += calculated_force
-            bodies[j].force -= calculated_force
+            bodies[j].force += Vec2(0, 0)-calculated_force
     for i, body in enumerate(bodies):
         body.apply_force()
 
@@ -38,9 +43,9 @@ class Body(Particle):
 
 def main():
     bodies =  []
-    for i in range(0, 2):
+    for i in range(0, 6):
         init_pos = Vec2(random.uniform(-200, 200), random.uniform(-200, 200))
-        init_vel = Vec2(random.uniform(-50, 50), random.uniform(-50, 50))
+        init_vel = Vec2(random.uniform(-25, 25), random.uniform(-25, 25))
         new_body = Body(random.uniform(3, 6), init_pos, init_vel, Vec2(0, 0))
         bodies.append(new_body)
     bodies.append(Body(1000, Vec2(0, 0), Vec2(0, 0), Vec2(0, 0)))
